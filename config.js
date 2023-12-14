93f5266c2b5e8e377444e2149a66890182a370c6
Anuragkumar747 <128835939+Anuragkumar747@users.noreply.github.com>
2023-12-14 12:53:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['13304083602', 'ANURAG KUMAR', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
